e6ef49426dd7d58c25b380faa0068cef8f2bd275
nwtas <115685597+nwtas@users.noreply.github.com>
2023-10-24 18:58:23 +1100

    Fixed data issue on voices API request

--- get_voices.js
+++ get_voices.js
@@ -29,20 +29,47 @@ const options = {
 const req = https.request(options, res => {
     console.log(`statusCode: ${res.statusCode}`)
     console.log(res.headers)
+    // Variable to store the received data
+    let data = '';
+    // Event handler for receiving data chunks
+    // This is required due to the large size of the response from d-id
+    res.on('data', (chunk) => {
+        // Append the data chunk to the existing data
+        data += chunk;
+        // check is the data size matches the content-length header
+        const sizeInBytes = Buffer.byteLength(data, 'utf8');
+        const contentLength = res.headers['content-length'];
+        console.log(`Received ${sizeInBytes} bytes of data`);
+        console.log(`Content-Length: ${contentLength}`);
+        if (sizeInBytes == contentLength) {
+            console.log('All data received');
+        }
+    });
+    // Event handler for the end of the response
+    res.on('end', () => {
+        // Calculate the size of the received data
+        const sizeInBytes = Buffer.byteLength(data, 'utf8');
+        console.log(`Return result size: ${sizeInBytes} bytes`);
+        process.stdout.write(JSON.stringify(JSON.parse(data), null, 2));
 
-    res.on('data', d => {
-        process.stdout.write(d)
-        // make d a JSON object and print pretty
-        process.stdout.write(JSON.stringify(JSON.parse(d), null, 2));
+    });
+});
+// Event handler for error handling
+req.on('error', (error) => {
+    console.error(error);
+});
 
-    })
-}
-)
+// End the request
+/* In Node.js, after creating an HTTP request using the http.request() method, you need to call req.end()
+to indicate that you have finished configuring the request and that it should be sent to the server.
 
-req.on('error', error => {
-    console.error(error)
-}
-)
+The req.end() method does the following:
+1. Sends the actual request to the server.
+2. Marks the end of the request configuration and begins the process of sending the request.
+3. Once the request has been sent, any further modifications or changes to the request are ignored.
+It is important to note that req.end() must be called for the request to be actually sent, otherwise, 
+the request will remain pending and will never be sent to the server.
+*/
 
-req.end()
+req.end();
 
